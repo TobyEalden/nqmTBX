923150f5dd1d855cc86dfd762ed417a3b3a76dfa
tobyealden <toby.ealden@gmail.com>
2015-07-01 15:30:06 +0100

    Add fixed side bar

--- app/client/main.js
+++ app/client/main.js
@@ -1,21 +1,23 @@
 
 var themes = {
   "blue-grey": {
-    mainColour: "blue-grey z-depth-1",
+    mainColour: "blue-grey lighten-1 white-text z-depth-1",
     menuTextColour: "grey-text text-lighten-3",
     mainTextColour: "white-text",
     visualisationBackground: "grey darken-3 grey-text text-lighten-1 z-depth-3",
     visualisationTitle: "white-text text-lighten-2",
     background: "blue-grey darken-2 grey-text text-lighten-5",
-    actionButton: "amber darken-2 white-text z-depth-2"
+    actionButton: "amber darken-2 white-text z-depth-2",
+    sideNav: "blue-grey lighten-2 white-text z-depth-0"
   },
   "indigo": {
-    mainColour: "indigo  z-depth-2",
+    mainColour: "indigo z-depth-1",
     menuTextColour: "grey-text text-lighten-3",
     mainTextColour: "white-text",
     visualisationBackground: "grey lighten-3 grey-text text-darken-1 z-depth-2",
     background: "grey lighten-2 grey-text text-darken-4",
-    actionButton: "red white-text"
+    actionButton: "red white-text",
+    sideNav: "indigo lighten-1 white-text z-depth-0"
   },
   "teal": {
     mainColour: "teal darken-2 z-depth-3",
@@ -24,7 +26,8 @@ var themes = {
     visualisationBackground: "grey darken-3 grey-text text-lighten-1 z-depth-2",
     visualisationTitle: "teal-text text-darken-4",
     background: "grey lighten-2 grey-text text-darken-3",
-    actionButton: "red white-text"
+    actionButton: "red white-text",
+    sideNav: "teal darken-1 grey-text text-lighten-3 z-depth-0"
   },
   "amber": {
     mainColour: "amber darken-1 z-depth-2",
@@ -32,7 +35,8 @@ var themes = {
     mainTextColour: "deep-orange-text text-darken-4",
     visualisationBackground: "grey darken-3 grey-text text-lighten-1 z-depth-2",
     background: "grey darken-2 grey-text text-lighten-5",
-    actionButton: "red white-text"
+    actionButton: "red white-text",
+    sideNav: "amber black-text z-depth-0"
   },
   "grey": {
     mainColour: "grey darken-1 z-depth-2",
@@ -41,7 +45,8 @@ var themes = {
     visualisationBackground: "grey darken-3 grey-text text-lighten-1 z-depth-2",
     visualisationTitle: "orange-text",
     background: "grey darken-2 grey-text text-lighten-5",
-    actionButton: "red white-text"
+    actionButton: "red white-text",
+    sideNav: "grey grey-text text-lighten-2 z-depth-0"
   }
 };
 
--- app/client/templates/layouts/mainLayout/mainLayout.js
+++ app/client/templates/layouts/mainLayout/mainLayout.js
@@ -6,7 +6,7 @@ Template.mainLayout.events({
 
 Template.mainLayout.onRendered(function() {
   $(".button-collapse").sideNav({
-    closeOnClick: true
+    closeOnClick: false
   });
 
   $('.nqm-user-menu-dropdown').dropdown({
@@ -18,5 +18,10 @@ Template.mainLayout.onRendered(function() {
       belowOrigin: true // Displays dropdown below the button
     }
   );
+
+  $('.collapsible').collapsible({
+    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+
 });
 
