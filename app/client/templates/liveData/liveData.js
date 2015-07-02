717a4ef9661e4cc1e9bfdbee8a044277bdb35b3b
tobyealden <toby.ealden@gmail.com>
2015-07-02 15:22:04 +0100

    Implement basic search by tags

--- app/client/templates/liveData/liveData.js
+++ app/client/templates/liveData/liveData.js
@@ -1,26 +1,26 @@
 /**
- * Created by toby on 01/07/15.
+ * Created by toby on 02/07/15.
  */
 
 /*****************************************************************************/
 /* Event Handlers */
 /*****************************************************************************/
-Template.visualise.events({
-  "input, change, paste, keyup, mouseup #nqm-visualisation-search": function(e, template) {
+Template.liveData.events({
+  "input, change, paste, keyup, mouseup #nqm-feed-search": function(e, template) {
     var searchTerm = new RegExp(e.target.value,"gi");
     var matches = feeds.find({ $or: [ {name: searchTerm}, {description: searchTerm }, {tags: searchTerm }]}).fetch();
-    Session.set("visualisation-search", matches);
+    Session.set("feed-search", matches);
   },
   "click .nqm-feed-card": function(event, template) {
-    Router.go("visualisation");
+    Router.go("liveData");
   }
 });
 
 /*****************************************************************************/
 /* Helpers */
 /*****************************************************************************/
-Template.visualise.helpers({
+Template.liveData.helpers({
   searchResults: function() {
-    return Session.get("visualisation-search");
+    return Session.get("feed-search");
   }
 });
